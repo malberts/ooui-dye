246b34a4f6d7da1413dd6d5504b7ddd74ed785ef
malberts <malberts@users.noreply.github.com>
2021-11-01 05:17:47 +0000

    Generate v0.42.0

--- dist/resources/ooui/oojs-ui-dye.js
+++ dist/resources/ooui/oojs-ui-dye.js
@@ -1,12 +1,12 @@
 /*!
- * OOUI v0.41.3
+ * OOUI v0.42.0
  * https://www.mediawiki.org/wiki/OOUI
  *
  * Copyright 2011–2021 OOUI Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2021-10-31T15:06:03Z
+ * Date: 2021-11-01T05:17:30Z
  */
 ( function ( OO ) {
 
