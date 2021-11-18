5e020817f30ab91b193df521ab512e40b666258d
Morne Alberts <morne@malberts.net>
2021-11-18 22:36:14 +0200

    Add border radius

--- convert.sh
+++ convert.sh
@@ -28,7 +28,15 @@ sed -Ei 's|^(@border-)([^:]*)(:.*)@wmui-color-[a-z0-9]+|\1\2\3var( --border-colo
 # Comment unused variables
 sed -Ei 's|^@(wmui\|width-breakpoint)|// @\1|g' $base/dye-ui-base.less
 
-# Unusable: mix()
+##
+# Specific variables
+##
+sed -Ei 's|^@(border-radius-base):(\s*)[^;]*;|@\1:\2var( --\1 );|g' $base/dye-ui-base.less
+
+##
+# Unusable mixins
+##
+# mix()
 # v0.39.3
 sed -i 's|mix( @color-progressive--active, @background-color-filled--disabled, 35% )|var( --color-primary--active--disabled )|g' $base/elements.less
 sed -i 's|mix( @color-progressive--active, @background-color-filled--disabled, 35% )|var( --color-primary--active--disabled )|g' $base/tools.less
@@ -36,7 +44,7 @@ sed -i 's|mix( @color-progressive--active, @background-color-filled--disabled, 3
 sed -i 's|mix( @color-primary--active, @background-color-filled--disabled, 35% )|var( --color-primary--active--disabled )|g' $base/elements.less
 sed -i 's|mix( @color-primary--active, @background-color-filled--disabled, 35% )|var( --color-primary--active--disabled )|g' $base/tools.less
 
-# Unusable: lighten()
+# lighten()
 # - function
 sed -Ei 's|(.mw-framed-button-colored.*, @active,)|\1 @active-bg,|g' $base/common.less
 sed -Ei 's|(.mw-tool-colored.*, @active,)|\1 @active-bg,|g' $base/common.less
@@ -46,7 +54,7 @@ sed -i 's|lighten( @active, 60% )|@active-bg|g' $base/common.less
 sed -Ei 's|(.mw-framed-button-colored.*@color-)(.*)(--active,)|\1\2\3 @background-color-\2,|g' $base/elements.less
 sed -Ei 's|(.mw-tool-colored.*@color-)(.*)(--active,)|\1\2\3 @background-color-\2,|g' $base/tools.less
 
-# Unusable: darken()
+# darken()
 sed -i 's|darken( @border-color-base, 14% )|@border-color-base--active|g' $base/widgets.less;
 
 # Replace import
