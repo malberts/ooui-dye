442381b1b84426970930d3670cdd7610332269bb
Morne Alberts <morne@malberts.net>
2021-11-20 07:53:29 +0200

    Rename variables file

--- convert.sh
+++ convert.sh
@@ -7,12 +7,12 @@
 base=oojs-ui/src/themes/dye
 
 # Copy base variables
-cp oojs-ui/node_modules/wikimedia-ui-base/wikimedia-ui-base.less $base/dye-ui-base.less
+cp oojs-ui/node_modules/wikimedia-ui-base/wikimedia-ui-base.less $base/variables.less
 
 # Replace variables with CSS Properties
-sed -Ei 's|^@(color-[^:]+):(\s*)[^;]*;|@\1:\2var( --\1 );|g' $base/dye-ui-base.less
-sed -Ei 's|^@(background-color-[^:]+):(\s*)[^;]*;|@\1:\2var( --\1 );|g' $base/dye-ui-base.less
-sed -Ei 's|^@(border-color-[^:]+):(\s*)[^;]*;|@\1:\2var( --\1 );|g' $base/dye-ui-base.less
+sed -Ei 's|^@(color-[^:]+):(\s*)[^;]*;|@\1:\2var( --\1 );|g' $base/variables.less
+sed -Ei 's|^@(background-color-[^:]+):(\s*)[^;]*;|@\1:\2var( --\1 );|g' $base/variables.less
+sed -Ei 's|^@(border-color-[^:]+):(\s*)[^;]*;|@\1:\2var( --\1 );|g' $base/variables.less
 
 sed -Ei 's|^@(color-[^:]+):(\s*).*@wmui[^;]*;|@\1:\2var( --\1 );|g' $base/common.less
 sed -Ei 's|^@(background-color-[^:]+):(\s*).*@wmui[^;]*;|@\1:\2var( --\1 );|g' $base/common.less
@@ -20,18 +20,18 @@ sed -Ei 's|^@(border-color-[^:]+):(\s*).*@wmui[^;]*;|@\1:\2var( --\1 );|g' $base
 
 # Hardcoded use of colors
 # v0.39.3
-sed -Ei 's|^(.*:.*)@wmui-color-yellow50|\1var( --border-color-warning )|g' $base/dye-ui-base.less
+sed -Ei 's|^(.*:.*)@wmui-color-yellow50|\1var( --border-color-warning )|g' $base/variables.less
 # v0.40.0
-sed -Ei 's|^(.*:.*)@wmui-color-accent50|\1var( --color-primary )|g' $base/dye-ui-base.less
+sed -Ei 's|^(.*:.*)@wmui-color-accent50|\1var( --color-primary )|g' $base/variables.less
 sed -Ei 's|^(@border-)([^:]*)(:.*)@wmui-color-[a-z0-9]+|\1\2\3var( --border-color-\2 )|g' $base/common.less
 
 # Comment unused variables
-sed -Ei 's|^@(wmui\|width-breakpoint)|// @\1|g' $base/dye-ui-base.less
+sed -Ei 's|^@(wmui\|width-breakpoint)|// @\1|g' $base/variables.less
 
 ##
 # Specific variables
 ##
-sed -Ei 's|^@(border-radius-base):(\s*)[^;]*;|@\1:\2var( --\1 );|g' $base/dye-ui-base.less
+sed -Ei 's|^@(border-radius-base):(\s*)[^;]*;|@\1:\2var( --\1 );|g' $base/variables.less
 
 ##
 # Unusable mixins
@@ -58,7 +58,7 @@ sed -Ei 's|(.mw-tool-colored.*@color-)(.*)(--active,)|\1\2\3 @background-color-\
 sed -i 's|darken( @border-color-base, 14% )|@border-color-base--active|g' $base/widgets.less;
 
 # Replace import
-sed -Ei "s|'.*wikimedia-ui-base.less'|'dye-ui-base.less'|g" $base/common.less
+sed -Ei "s|'.*wikimedia-ui-base.less'|'variables.less'|g" $base/common.less
 
 # Convert all images to black, except the white variants
 find $base -type f -name '*.json' -exec sed -Ei '/"#fff"/! s|color": "#[^"]*"|color": "#000"|g' {} \;
