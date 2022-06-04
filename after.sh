2a8753701ba209ffc103c7daaf68f0760cafbe5d
Morne Alberts <morne@malberts.net>
2022-06-04 18:08:54 +0200

    Fix negative pixels again

--- after.sh
+++ after.sh
@@ -4,6 +4,7 @@ find oojs-ui/dist -name *.css \
   -exec echo "{}" \; \
   `# Pixel values > 2px` \
   `# Replace larger sizes with ratio = (x / @ooui-font-size-browser) / @ooui-font-size-base = 14` \
-  -exec sed -Ei 's#\b(-[3-9]|-[1-9][0-9]+)px#calc(\1 / 14 * var(--font-size-base))#g' {} \; \
+  -exec sed -Ei 's#-\b([3-9]|[1-9][0-9]+)px#calc(-\1 / 14 * var(--font-size-base))#g' {} \; \
+  -exec sed -Ei 's#\b([3-9]|[1-9][0-9]+)px#calc(\1 / 14 * var(--font-size-base))#g' {} \; \
   `# Remove box-sizing` \
   -exec sed -i '/box-sizing: border-box/d' {} \;
